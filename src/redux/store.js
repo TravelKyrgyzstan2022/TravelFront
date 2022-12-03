131f9ac286c9bba91ece1d8f27ea8901d8ab1d3a
TravelKyrgyzstan2022 <115484543+TravelKyrgyzstan2022@users.noreply.github.com>
2022-12-03 15:00:16 +0600

    Delete src/redux directory

--- src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import  counterSlice  from "./reducers/generalSlice";
-
-export const store = configureStore({
-  reducer: {
-    general: counterSlice,
-  },
-});
